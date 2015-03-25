6be5e5bc9e68371c65f38efe01a7330858f91874
JNPA <jnpa@campus.ul.pt>
2015-03-25 15:40:42 +0000

    Connect the creation of the degree site with the creation of the degree object

--- src/main/java/org/fenixedu/learning/task/LoadAllDegreeSites.java
+++ src/main/java/org/fenixedu/learning/task/LoadAllDegreeSites.java
@@ -1,38 +1,34 @@
 package org.fenixedu.learning.task;
 
-import static com.google.common.base.Joiner.on;
-
 import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.bennu.core.domain.Bennu;
-import org.fenixedu.bennu.portal.domain.MenuContainer;
-import org.fenixedu.bennu.portal.domain.PortalConfiguration;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
-import org.fenixedu.cms.domain.CMSFolder;
 import org.fenixedu.learning.domain.degree.DegreeSite;
 import org.fenixedu.learning.domain.degree.DegreeSiteListener;
 
 public class LoadAllDegreeSites extends CustomTask {
 
-	@Override
-	public void runTask() throws Exception {
-		for (Degree degree : Bennu.getInstance().getDegreesSet()) {
-			/*
-			 * Since all Degree instances were created by script, no signal
-			 * was raised, and therefore no LMS structures were created.
-			 */
-			if (degree.getSite() == null) {
-				DegreeSite s = DegreeSiteListener.create(degree);
-				s.setFolder(folderForPath(PortalConfiguration.getInstance().getMenu(), "degrees"));
-				s.setSlug(on("-").join(degree.getSigla(), degree.getExternalId()));
-				System.out.println(degree.getPresentationName());
-			}
-		}
-	}
-	
-	private CMSFolder folderForPath(MenuContainer parent, String path) {
-
-        return parent.getOrderedChild().stream().filter(item -> item.getPath().equals(path))
-                .map(item -> item.getAsMenuFunctionality().getCmsFolder()).findFirst().orElseThrow(() -> new RuntimeException("no.degree.site.folder.was.found"));
+    @Override
+    public void runTask() throws Exception {
+        for (Degree degree : Bennu.getInstance().getDegreesSet()) {
+            /*
+             * Since all Degree instances were created by script, no signal
+             * was raised, and therefore no LMS structures were created.
+             */
+            if (degree.getSite() == null) {
+                DegreeSite s = DegreeSiteListener.create(degree);
+                /*This lines are now inside the constructor*/
+                //s.setFolder(folderForPath(PortalConfiguration.getInstance().getMenu(), "degrees"));
+                //s.setSlug(on("-").join(degree.getSigla(), degree.getExternalId()));
+                System.out.println(degree.getPresentationName());
+            }
+        }
     }
+
+//	private CMSFolder folderForPath(MenuContainer parent, String path) {
+//
+//        return parent.getOrderedChild().stream().filter(item -> item.getPath().equals(path))
+//                .map(item -> item.getAsMenuFunctionality().getCmsFolder()).findFirst().orElseThrow(() -> new RuntimeException("no.degree.site.folder.was.found"));
+//    }
 
 }
