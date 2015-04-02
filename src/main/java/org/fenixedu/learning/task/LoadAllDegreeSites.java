8c56dbd75e8bbc6af9bee1ff78bf0c6c02de87b7
JoaoRoxo <joao.roxo@qub-it.com>
2015-04-02 14:49:39 +0100

    Generalized Learning Theme to make it more easy to customize.

--- src/main/java/org/fenixedu/learning/task/LoadAllDegreeSites.java
+++ src/main/java/org/fenixedu/learning/task/LoadAllDegreeSites.java
@@ -8,19 +8,27 @@ import org.fenixedu.learning.domain.degree.DegreeSiteListener;
 
 public class LoadAllDegreeSites extends CustomTask {
 
-	@Override
-	public void runTask() throws Exception {
-		for (Degree degree : Bennu.getInstance().getDegreesSet()) {
-			/*
-			 * Since all Degree instances were created by script, no signal
-			 * was raised, and therefore no LMS structures were created.
-			 */
-			if (degree.getSite() == null) {
-				DegreeSite s = DegreeSiteListener.create(degree);
-				s.setFolder(Bennu.getInstance().getCmsFolderSet().iterator().next());
-				System.out.println(degree.getPresentationName());
-			}
-		}
-	}
+    @Override
+    public void runTask() throws Exception {
+        for (Degree degree : Bennu.getInstance().getDegreesSet()) {
+            /*
+             * Since all Degree instances were created by script, no signal
+             * was raised, and therefore no LMS structures were created.
+             */
+            if (degree.getSite() == null) {
+                DegreeSite s = DegreeSiteListener.create(degree);
+                /*This lines are now inside the constructor*/
+                //s.setFolder(folderForPath(PortalConfiguration.getInstance().getMenu(), "degrees"));
+                //s.setSlug(on("-").join(degree.getSigla(), degree.getExternalId()));
+                System.out.println(degree.getPresentationName());
+            }
+        }
+    }
+
+//	private CMSFolder folderForPath(MenuContainer parent, String path) {
+//
+//        return parent.getOrderedChild().stream().filter(item -> item.getPath().equals(path))
+//                .map(item -> item.getAsMenuFunctionality().getCmsFolder()).findFirst().orElseThrow(() -> new RuntimeException("no.degree.site.folder.was.found"));
+//    }
 
 }
